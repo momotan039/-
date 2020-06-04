7c6ac81acba1c0c3540ffdab75338b55ae6ea1f8
momotan039 <66243365+momotan039@users.noreply.github.com>
2020-06-04 10:12:54 +0300

    Update select anime type.js

--- select anime type.js
+++ select anime type.js
@@ -16,7 +16,13 @@ optionsList.forEach(o => {
     selected.innerHTML = o.querySelector("label").innerHTML;
     
     optionsContainer.classList.remove("active");
-   
+    if(selected.innerHTML=="الكل"){
+
+location.href="انمي?&max-results=20";
+    }
+    else{
+     location.href=selected.innerHTML+"?&max-results=20";
+    }
       
         
   });
