39cfcccda7a082bf2cb97a4e962f0e5fe01940da
momotan039 <66243365+momotan039@users.noreply.github.com>
2020-06-04 10:53:14 +0300

    Update select anime type.js

--- select anime type.js
+++ select anime type.js
@@ -13,7 +13,7 @@ selected.addEventListener("click", () => {
 
 optionsList.forEach(o => {
   o.addEventListener("click", () => {
-    selected.innerHTML = o.querySelector("label").innerHTML;
+    selected.innerHTML = o.querySelector("label").innerHTML; 
     
     optionsContainer.classList.remove("active");
 
